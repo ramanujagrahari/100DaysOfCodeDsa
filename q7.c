392b16793605ad252b90a03bc3c104629870d4d7
ramanujagrahari <ramanuj.ag.6233@gmail.com>
2026-02-09 04:05:26 +0000

    Write a recursive function fib(n) to compute the n-th  Fibonacci number where fib(0)=0 and fib(1)=1

---
+++ q7.c
@@ -0,0 +1,25 @@
+/* Write a recursive function fib(n) to compute the n-th
+ Fibonacci number where fib(0)=0 and fib(1)=1*/
+ #include <stdio.h>
+
+// Recursive function to find nth Fibonacci number
+int fib(int n) {
+    if (n == 0)
+        return 0;
+    else if (n == 1)
+        return 1;
+    else
+        return fib(n - 1) + fib(n - 2);
+}
+
+int main() {
+    int n;
+
+    // Read input
+    scanf("%d", &n);
+
+    // Print nth Fibonacci number
+    printf("%d\n", fib(n));
+
+    return 0;
+}
