a5bd07ca2d3eb6fde0e03f0d929456a808a64112
ramanujagrahari <ramanuj.ag.6233@gmail.com>
2026-04-16 12:00:00 +0530

    Day 83 - Implement Selection Sort

---
+++ q83.c
@@ -0,0 +1,38 @@
+/* Day 83 - Question 1: Implement Selection Sort
+   Sort an array in ascending order using Selection Sort.
+   Finds the minimum element in each pass and places it at the correct position. */
+#include <stdio.h>
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIdx = i;
+
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIdx])
+                minIdx = j;
+        }
+
+        if (minIdx != i) {
+            int temp = arr[i];
+            arr[i] = arr[minIdx];
+            arr[minIdx] = temp;
+        }
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    selectionSort(arr, n);
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    return 0;
+}
