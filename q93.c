1899c77943db54f1f3b68579f88ed4842ef22bc9
ramanujagrahari <ramanuj.ag.6233@gmail.com>
2026-04-26 12:00:00 +0530

    Day 93 - Insertion Sort Implementation

---
+++ q93.c
@@ -0,0 +1,34 @@
+/* Day 93 - Question 1: Insertion Sort Implementation
+   Sort an array using Insertion Sort.
+   Builds the sorted portion one element at a time. */
+#include <stdio.h>
+
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    insertionSort(arr, n);
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    return 0;
+}
