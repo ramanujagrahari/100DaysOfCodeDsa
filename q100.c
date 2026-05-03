47caf4cff8759e776749c83f3cc908ef22f7e9cb
ramanujagrahari <ramanuj.ag.6233@gmail.com>
2026-05-03 12:00:00 +0530

    Day 100 - Count Inversions in Array (Final Challenge)

---
+++ q100.c
@@ -0,0 +1,56 @@
+/* Day 100 - Question 1: Count Inversions in Array
+   Count the number of inversions in an array using a modified Merge Sort.
+   An inversion is a pair (i, j) where i < j but arr[i] > arr[j].
+   This is the final challenge of the 100 Days of DSA! */
+#include <stdio.h>
+#include <stdlib.h>
+
+long long merge(int arr[], int temp[], int left, int mid, int right) {
+    int i = left, j = mid, k = left;
+    long long inv_count = 0;
+
+    while (i <= mid - 1 && j <= right) {
+        if (arr[i] <= arr[j]) {
+            temp[k++] = arr[i++];
+        } else {
+            temp[k++] = arr[j++];
+            inv_count += (mid - i); /* All remaining left elements form inversions */
+        }
+    }
+
+    while (i <= mid - 1) temp[k++] = arr[i++];
+    while (j <= right) temp[k++] = arr[j++];
+
+    for (i = left; i <= right; i++) arr[i] = temp[i];
+
+    return inv_count;
+}
+
+long long mergeSort(int arr[], int temp[], int left, int right) {
+    long long inv_count = 0;
+    if (right > left) {
+        int mid = (left + right) / 2;
+
+        inv_count += mergeSort(arr, temp, left, mid);
+        inv_count += mergeSort(arr, temp, mid + 1, right);
+        inv_count += merge(arr, temp, left, mid + 1, right);
+    }
+    return inv_count;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int* temp = (int*)malloc(n * sizeof(int));
+
+    long long result = mergeSort(arr, temp, 0, n - 1);
+    printf("%ld\n", (long)result);
+
+    free(temp);
+    return 0;
+}
